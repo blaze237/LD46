a52370f6fc229d52e062e31d35d9207162f75eb2
SecretEtude <40235281+SecretEtude@users.noreply.github.com>
2020-04-18 16:38:16 +0100

    Audio assets for Scene_001
    Karen prefab with higheel sounds
    Night CityAmbience (should suit better when skybox is changed to night one)
    1st batch of Zombie sounds (not in scene yet)

--- Unity Project/LD46/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity Project/LD46/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,54 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_16___MAY_CONTAIN_METAL = 3033383690U;
         static const AkUniqueID PLAY_AMBIENTTEST = 4029336046U;
+        static const AkUniqueID PLAY_CITY_BED = 1966587683U;
         static const AkUniqueID PLAY_ITEMTEST = 1408766231U;
         static const AkUniqueID PLAY_PLAYERTEST = 3141342825U;
+        static const AkUniqueID PLAY_WALKING_HIGHHEEL = 3555340024U;
+        static const AkUniqueID PLAY_ZOMBIE_LARGE = 2540355032U;
+        static const AkUniqueID PLAY_ZOMBIE_MEDIUM = 1868838038U;
+        static const AkUniqueID PLAY_ZOMBIE_SMALL = 833498416U;
+        static const AkUniqueID STOP_CITY_BED = 210502101U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace SPACE
+        {
+            static const AkUniqueID GROUP = 4164838345U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CLOSE = 1451272583U;
+                static const AkUniqueID FAR = 1183803292U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace SPACE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SOUNDTRACK
+        {
+            static const AkUniqueID GROUP = 2082426231U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FRONTEND = 4212191649U;
+                static const AkUniqueID LEVEL01 = 3152323980U;
+            } // namespace SWITCH
+        } // namespace SOUNDTRACK
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTANCETOLISTENER = 2352475335U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -26,8 +70,16 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID ENEMIES = 2242381963U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID ZOMBIES = 4113933398U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID KARENSTEPS = 2225273953U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
