452b64a4ad9c3b6dd654e1e134fdb1ca8056e8f1
SecretEtude <mchuong91@hotmail.com>
2020-04-21 00:08:06 +0100

    fk u wwiseguy

---
+++ LudumDare2020_Audiosource/LD2020/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,107 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_CITY_BED = 1966587683U;
+        static const AkUniqueID PLAY_FLAMINGBARREL_LOOP = 732558559U;
+        static const AkUniqueID PLAY_GENERATOR_LOOP = 3692090582U;
+        static const AkUniqueID PLAY_ITEMTEST = 1408766231U;
+        static const AkUniqueID PLAY_MUSIC_LEVEL01 = 3711381197U;
+        static const AkUniqueID PLAY_PHONE_BATTERYDEAD = 1100482298U;
+        static const AkUniqueID PLAY_PHONE_LOWBATTERY = 3837069120U;
+        static const AkUniqueID PLAY_PICKUP_BATTERY = 4135255572U;
+        static const AkUniqueID PLAY_PICKUP_COFFEE = 581921785U;
+        static const AkUniqueID PLAY_PICKUP_PETROL = 3202284035U;
+        static const AkUniqueID PLAY_PLAYERTEST = 3141342825U;
+        static const AkUniqueID PLAY_REPAIRTOWER = 881265840U;
+        static const AkUniqueID PLAY_WALKING_HIGHHEEL = 3555340024U;
+        static const AkUniqueID PLAY_ZOMBIE = 246154100U;
+        static const AkUniqueID PLAY_ZOMBIE_LARGE = 2540355032U;
+        static const AkUniqueID PLAY_ZOMBIE_MEDIUM = 1868838038U;
+        static const AkUniqueID PLAY_ZOMBIE_SMALL = 833498416U;
+        static const AkUniqueID PLAY_ZOMBIEDAN_GROWL = 1725534455U;
+        static const AkUniqueID PLAY_ZOMBIES = 2362011631U;
+        static const AkUniqueID STOP_CITY_BED = 210502101U;
+        static const AkUniqueID STOP_FLAMINGBARREL_LOOP = 3824269441U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace SPACE
+        {
+            static const AkUniqueID GROUP = 4164838345U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CLOSE = 1451272583U;
+                static const AkUniqueID FAR = 1183803292U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace SPACE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SOUNDTRACK
+        {
+            static const AkUniqueID GROUP = 2082426231U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FRONTEND = 4212191649U;
+                static const AkUniqueID LEVEL01 = 3152323980U;
+            } // namespace SWITCH
+        } // namespace SOUNDTRACK
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTANCETOLISTENER = 2352475335U;
+        static const AkUniqueID M_HEALTH = 3613647679U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID ITEMS = 2151963051U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID ZOMBIES = 4113933398U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID GENERALVERB = 2827677392U;
+        static const AkUniqueID KARENSTEPS = 2225273953U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
--- Unity Project/LD46/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity Project/LD46/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,17 +13,27 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_16___MAY_CONTAIN_METAL = 3033383690U;
-        static const AkUniqueID PLAY_AMBIENTTEST = 4029336046U;
         static const AkUniqueID PLAY_CITY_BED = 1966587683U;
+        static const AkUniqueID PLAY_FLAMINGBARREL_LOOP = 732558559U;
+        static const AkUniqueID PLAY_GENERATOR_LOOP = 3692090582U;
         static const AkUniqueID PLAY_ITEMTEST = 1408766231U;
+        static const AkUniqueID PLAY_MUSIC_LEVEL01 = 3711381197U;
+        static const AkUniqueID PLAY_PHONE_BATTERYDEAD = 1100482298U;
+        static const AkUniqueID PLAY_PHONE_LOWBATTERY = 3837069120U;
+        static const AkUniqueID PLAY_PICKUP_BATTERY = 4135255572U;
+        static const AkUniqueID PLAY_PICKUP_COFFEE = 581921785U;
+        static const AkUniqueID PLAY_PICKUP_PETROL = 3202284035U;
         static const AkUniqueID PLAY_PLAYERTEST = 3141342825U;
+        static const AkUniqueID PLAY_REPAIRTOWER = 881265840U;
         static const AkUniqueID PLAY_WALKING_HIGHHEEL = 3555340024U;
+        static const AkUniqueID PLAY_ZOMBIE = 246154100U;
         static const AkUniqueID PLAY_ZOMBIE_LARGE = 2540355032U;
         static const AkUniqueID PLAY_ZOMBIE_MEDIUM = 1868838038U;
         static const AkUniqueID PLAY_ZOMBIE_SMALL = 833498416U;
         static const AkUniqueID PLAY_ZOMBIEDAN_GROWL = 1725534455U;
+        static const AkUniqueID PLAY_ZOMBIES = 2362011631U;
         static const AkUniqueID STOP_CITY_BED = 210502101U;
+        static const AkUniqueID STOP_FLAMINGBARREL_LOOP = 3824269441U;
     } // namespace EVENTS
 
     namespace STATES
@@ -61,6 +71,7 @@ namespace AK
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID DISTANCETOLISTENER = 2352475335U;
+        static const AkUniqueID M_HEALTH = 3613647679U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -72,13 +83,16 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMBIENT = 77978275U;
-        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID ITEMS = 2151963051U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID ZOMBIES = 4113933398U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
     {
+        static const AkUniqueID GENERALVERB = 2827677392U;
         static const AkUniqueID KARENSTEPS = 2225273953U;
     } // namespace AUX_BUSSES
 
