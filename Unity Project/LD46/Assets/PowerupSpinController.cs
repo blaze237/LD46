5bc65b5dcddd2ef608bb12e5bfdb022eb88ecb21
S4918542 <jem3006@gmail.com>
2020-04-20 18:17:52 +0100

    Added pickup prefabs

---
+++ Unity Project/LD46/Assets/PowerupSpinController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpinController : MonoBehaviour
+{
+    [SerializeField] Vector3 spinAxis;
+    float spinSpeed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation *= Quaternion.Euler(spinAxis * Time.deltaTime * spinSpeed);
+    }
+}
