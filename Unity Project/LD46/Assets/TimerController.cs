d54cd01924cd3162ebd04e0f4d486b4d39a71db4
S4918542 <jem3006@gmail.com>
2020-04-20 21:31:34 +0100

    Added UI timer

---
+++ Unity Project/LD46/Assets/TimerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerController : MonoBehaviour
+{
+    TextMeshProUGUI timerText;
+    float startingTime;
+    int timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        startingTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = (int)Time.time - (int)startingTime;
+        timerText.SetText(timer.ToString() + "s");
+    }
+}
