dcbd4e9b6f71c67a21c811c336a8a1dea93b3b2d
Unknown <psydrw@nottingham.ac.uk>
2020-04-20 22:33:48 +0100

    ui

---
+++ Unity Project/LD46/Assets/FuelCanText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FuelCanText : MonoBehaviour
+{
+    TextMeshProUGUI txt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        Inventory inventory = Player.instance.GetInventory();
+
+        txt.SetText(inventory.m_fuelCans.ToString());
+
+        //   txt.
+    }
+
+
+
+}
---
+++ Unity Project/LD46/Assets/TowerFuelSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerFuelSlider : MonoBehaviour
+{
+    Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        slider.value = Tower.instance.m_fuel / 100.0f;
+    }
+}
