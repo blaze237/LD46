7a1d5d068ae3bd2aa5bdc07818767497a1b79dea
Unknown <psydrw@nottingham.ac.uk>
2020-04-20 09:41:36 +0100

    Added basic minimap system

---
+++ Unity Project/LD46/Assets/MinimapFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    public float hOffset = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = Player.instance.transform.position;
+        playerPos.y += hOffset;
+        transform.position = playerPos;
+    }
+}
